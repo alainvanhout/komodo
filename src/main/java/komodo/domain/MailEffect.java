f06161ba1a06bb293cd509333132c52d17d37e02
Alain Van Hout <alainvanhout@gmail.com>
2016-07-05 17:35:16 +0200

    Move mailing info to json config file

--- src/main/java/komodo/domain/MailEffect.java
+++ src/main/java/komodo/domain/MailEffect.java
@@ -1,5 +1,10 @@
 package komodo.domain;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import komodo.urlcheck.UrlCheck;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -7,18 +12,29 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class MailEffect implements Effect {
 
+    private Gson gson;
     private JavaMailSenderImpl sender;
+    private Map<String, String> params;
 
     @PostConstruct
-    public void init() {
+    public void init() throws FileNotFoundException {
+        gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+        params = toMap(new File("mailParams.json"));
+
         sender = new JavaMailSenderImpl();
-        sender.setHost("localhost");
-        sender.setPort(25);
+        sender.setHost(params.get("host"));
+        sender.setPort(Integer.parseInt(params.get("port")));
     }
 
     @Override
@@ -26,11 +42,23 @@ public class MailEffect implements Effect {
         try {
             MimeMessage message = sender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message);
-            helper.setTo("test@localhost");
+            helper.setTo(params.get("to"));
+            helper.setSubject(params.get("subject"));
             helper.setText(check.getStatus() + " - " + LocalTime.now());
             sender.send(message);
         } catch (MessagingException e) {
             e.printStackTrace();
+        }
+    }
+
+    private Map toMap(File file) throws FileNotFoundException {
+        JsonReader reader = null;
+        try {
+            reader = new JsonReader(new FileReader(file.getAbsolutePath()));
+            Map<String, String> params = gson.fromJson(reader, HashMap.class);
+            return params;
+        } finally {
+            IOUtils.closeQuietly(reader);
         }
     }
 
--- src/main/java/komodo/urlcheck/UrlCheckRunner.java
+++ src/main/java/komodo/urlcheck/UrlCheckRunner.java
@@ -1,11 +1,14 @@
 package komodo.urlcheck;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import komodo.domain.Check;
 import komodo.domain.Effect;
 import komodo.runners.CheckRunner;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.Duration;
@@ -19,31 +22,42 @@ public class UrlCheckRunner implements CheckRunner<UrlCheck> {
     public void run(UrlCheck check) {
         HttpURLConnection connection = null;
         try {
+            check.last(LocalDateTime.now());
+
             connection = (HttpURLConnection) new URL(check.getUrl()).openConnection();
             connection.setRequestMethod("GET");
 
-            if (check.getUsername() != null && check.getPassword() != null) {
-                String userCredentials = check.getUsername() + ":" + check.getPassword();
-                String basicAuth = "Basic " + new String(new Base64().encode(userCredentials.getBytes()));
-                connection.setRequestProperty("Authorization", basicAuth);
-            }
+            addAuthentication(check, connection);
 
             int responseCode = connection.getResponseCode();
-            check.last(LocalDateTime.now());
-
-            check.success(true);
-
             System.out.println(check.getUrl() + ": " + responseCode + " - " + LocalTime.now());
-        } catch (Exception e) {
-            check.success(false);
-            Effect failureEffect = check.getFailureEffect();
-            if (failureEffect != null){
-                failureEffect.run(check);
+
+            check.success(responseCode >= 200 && responseCode < 300);
+            if (!check.isSuccess()){
+                fail(check);
             }
+        } catch (Exception e) {
+            fail(check);
         } finally {
             if (connection != null) {
                 connection.disconnect();
             }
+        }
+    }
+
+    private void addAuthentication(UrlCheck check, HttpURLConnection connection) {
+        if (check.getUsername() != null && check.getPassword() != null) {
+            String userCredentials = check.getUsername() + ":" + check.getPassword();
+            String basicAuth = "Basic " + new String(new Base64().encode(userCredentials.getBytes()));
+            connection.setRequestProperty("Authorization", basicAuth);
+        }
+    }
+
+    private void fail(UrlCheck check) {
+        check.success(false);
+        Effect failureEffect = check.getFailureEffect();
+        if (failureEffect != null){
+            failureEffect.run(check);
         }
     }
 
