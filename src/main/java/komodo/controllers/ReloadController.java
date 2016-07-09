d34d156e9a0c8fb772430b06b4c00f25f9fdbd01
Alain Van Hout <alainvanhout@gmail.com>
2016-07-09 23:59:09 +0200

    Add reload button to overview page

--- src/main/java/komodo/controllers/ReloadController.java
+++ src/main/java/komodo/controllers/ReloadController.java
@@ -2,17 +2,19 @@ package komodo.controllers;
 
 import komodo.services.TimerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ReloadController {
 
     @Autowired
     private TimerService timerService;
 
     @RequestMapping("reload")
-    public void reload(){
+    public String reload(){
         timerService.reload();
+        return "redirect:/overview";
     }
 }
